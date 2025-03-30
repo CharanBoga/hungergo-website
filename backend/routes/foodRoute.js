bf0014c819495dbdc5350adfda2d247fad316292
charan <charanboga2003@gmail.com>
2025-03-30 19:08:49 +0530

    lol

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { addFood, foodList, removeFood, updateFood } from '../controllers/foodController.js';
-import upload from '../config/imageStorage.js'
 
 const foodRouter = express.Router();
 
